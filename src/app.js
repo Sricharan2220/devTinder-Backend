a821ce0e49b30a1452d16297cfa606a3768aaed2
Sricharan2220 <kschrn18615@gmail.com>
2025-08-18 18:24:47 +0530

    User Schema Validation Done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
+app.use(express.json());
+
+
+app.post("/signup",(req,res)=>{
+    const user = new User(req.body);
+
+    try{
+        user.save();
+        res.send("User saved Successfully");
+    }catch(err){
+        res.status(400).send("Error saving the User:" + err.message);
+    }
+})
+
+
 
 
 connectDB().then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,66 @@
 const mongoose = require('mongoose');
-
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
     firstName: { 
         type: String,
+        required: true,
+        minlength: 3
     },
     lastName: {
         type: String,
+        minlength: 3
     },
     emailId: {
         type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new err("Invalid Email Address: " + value);
+            }
+        }
     },
     password: {
         type: String,
+        required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new err("Enter Strong Password: " + value);
+            }
+        }
     },
     age: {
         type: Number,
+        min: 18
     },
     gender: {
         type: String,
+        validate(value){
+            if(!["male","female","others"].includes(value.lowercase)){
+                throw new err("Gender is not valid");
+            }
+        }
+    },
+    about:{
+        type: String,
+        default: "This is the default about the user"
+    },
+    photoUrl: {
+        type: String,
+        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fopenclipart.org%2Fdetail%2F346569%2Fdefault-silhouette-avatar&psig=AOvVaw1Yde2fWb5S52S-G3B54rRo&ust=1755602220997000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCNCk14melI8DFQAAAAAdAAAAABAE",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new err("Invalid Photo URL: " + value);
+            }
+        }
+    },
+    skills:{
+        type: [String],
+        maxlength: 20
     }
-});
+},
+{timestamps: true});
 
 module.exports = mongoose.model("User",userSchema);
