c490f7892f049819e25163496fd8399e00256e22
Sricharan2220 <kschrn18615@gmail.com>
2026-04-10 04:45:08 +0530

    feat: chat

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 const cors = require("cors");
-
+const http = require("http");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 const app = express();
+const server = http.createServer(app);
+initializeSocket(server);
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -23,13 +27,14 @@ app.use("/",authRouter);
 app.use("/",profileRouter);
 app.use("/",requestRouter);
 app.use("/",userRouter);
+app.use("/",chatRouter)
 
 
 const port = process.env.PORT || 3000;
 
 connectDB().then(()=>{
     console.log("Database Connection Established....");
-    app.listen(port,()=>{
+    server.listen(port,()=>{
         console.log(`Server running on port ${port}`);
     });
 }).catch((err)=>{
