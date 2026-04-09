6aa47971d2f8401b3d592daf55cf0bdf2452d12f
Sricharan2220 <kschrn18615@gmail.com>
2026-04-09 15:08:27 +0530

    Db fixes

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://kschrn2220:mSV6@QAc75VM@m.@cluster0.at5rqca.mongodb.net/?appName=devTinder");
+        await mongoose.connect("mongodb+srv://kschrn2220:qfT9njqHe20NWBiv@cluster0.at5rqca.mongodb.net/?appName=devTinder");
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection error:", error.message);
