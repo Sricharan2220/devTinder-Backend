b27cdd44c6bc4a526e2f3cc2b7821cfb5fe7b498
Sricharan2220 <kschrn18615@gmail.com>
2026-04-09 14:47:33 +0530

    dns fixes

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/devTinder");
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/devTinder");
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection error:", error.message);
+        throw error;
+    }
 };
 
 module.exports = connectDB
